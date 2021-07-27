18091f73e0f547028e817291b4258fc1f8e39d8b
boogameow <76601381+boogameow@users.noreply.github.com>
2021-07-27 15:06:41 -0700

    fix

--- Phase.lua
+++ Phase.lua
@@ -1,11 +1,17 @@
+-- services
+
 local cam = workspace.CurrentCamera
 local inputservice = game:GetService("UserInputService")
 local lighting = game:GetService("Lighting")
 local ps = game:GetService("Players")
 local pl = ps.LocalPlayer
 
-local clone, old, con, con2 = nil
+-- vars
+
+local clone, char = nil
 local phasing = false
+
+-- instances
 
 local cor = Instance.new("ColorCorrectionEffect") -- phasing color
 cor.Enabled = true
@@ -16,6 +22,8 @@ sound.SoundId = "rbxassetid://362395087"
 sound.Volume = 1
 sound.Looped = true
 
+-- misc functions
+
 local function setop(char, tr)
 	for _, v in pairs(char:GetChildren()) do
 		if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then
@@ -24,60 +32,84 @@ local function setop(char, tr)
 	end
 end
 
+local function setgui(st)
+	for _, v in pairs(pl.PlayerGui:GetChildren()) do
+		if v:IsA("ScreenGui") then
+			v.ResetOnSpawn = st
+		end
+	end
+end
+
+-- main functions
+
 local function destroy(tp)
 	if phasing == true then
-		if con then
-			con:Disconnect()
-			con2:Disconnect()
-			con, con2 = nil
-		end
-		
 		sound:Stop()
 		sound.Parent = nil
 		cor.Parent = nil
+		setgui(false)
 		
 		if tp == true then
-			old.HumanoidRootPart.CFrame = clone.HumanoidRootPart.CFrame
+			char.HumanoidRootPart.CFrame = clone.HumanoidRootPart.CFrame
 		end
 		
-		clone:Destroy()
-		pl.Character = old
-		cam.CameraSubject = old
+		clone.Parent = nil
+		pl.Character = char
+		cam.CameraSubject = char
 		
-		setop(old, 0)
+		setgui(true)
+		setop(char, 0)
 		
-		clone, old = nil
 		phasing = false
 	end
 end
 
 local function create()
-	if clone == nil and phasing == false and pl.Character and pl.Character.Humanoid.Health > 0 then -- fail-safes
+	if clone ~= nil and char ~= nil and phasing == false and char.Humanoid.Health > 0 then
 		cor.Parent = lighting
 		sound.Parent = workspace
 		sound:Play()
+		setgui(false)
 		
-		pl.Character.Archivable = true
-		old = pl.Character
-		clone = pl.Character:Clone()
-		
-		clone.HumanoidRootPart.CFrame += (clone.HumanoidRootPart.CFrame.LookVector * -3) -- move clone behind original
+		clone.HumanoidRootPart.CFrame = char.HumanoidRootPart.CFrame + (char.HumanoidRootPart.CFrame.LookVector * -3)
 		clone.Parent = workspace
 		
 		pl.Character = clone
 		cam.CameraSubject = clone.Humanoid
-
-		clone.Animate.Disabled = true -- fix animations on the new char
-		clone.Animate.Disabled = false
-		clone.Humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
-
-		con = old.Humanoid.Died:Connect(destroy) -- if the husk dies
-		con2 = pl.CharacterAdded:Connect(destroy) -- if LoadCharacter() is forced by the server
 		
-		setop(old, 0.5)
+		clone.Animate.Disabled = true
+		clone.Animate.Disabled = false -- fix animations
+		
+		setgui(true)
+		setop(char, 0.5)
 		phasing = true
 	end
 end
+
+-- character setup & cloning
+
+local function setupchar(detchar)
+	if clone then
+		clone:Destroy()
+		clone = nil
+	end
+
+	if phasing == true then
+		sound:Stop()
+		sound.Parent = nil
+		cor.Parent = nil
+		phasing = false
+	end
+
+	char = pl.Character
+	char.Archivable = true
+	clone = char:Clone()
+
+	clone:WaitForChild("Humanoid").DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+	char.Humanoid.Died:Connect(destroy)
+end
+
+-- input handling
 
 inputservice.InputBegan:Connect(function(input, proc)
 	if proc then return elseif input.KeyCode == Enum.KeyCode.F1 then
@@ -90,3 +122,11 @@ inputservice.InputBegan:Connect(function(input, proc)
 		destroy(true) -- F2 to end phase (teleport character to phaser)
 	end
 end)
+
+-- hooking events
+
+pl.CharacterAppearanceLoaded:Connect(setupchar)
+
+if pl.Character and pl:HasAppearanceLoaded() == true then
+	setupchar()
+end
